f3edf0f81ece8f0e57120bd70c21342da4bf3f3c
MTGarde <s22gardmart@venta.lv>
2024-03-13 18:00:01 +0200

    pascal triangle

---
+++ src/service/seminar1_string.java
@@ -0,0 +1,18 @@
+package service;
+
+public class seminar1_string {
+
+    public static void main(String[] args) {
+        String inputEquation = "1 - 3 * 18 / 4 + 2";
+
+    }
+
+    public static double executeStringEquation(String equation) throws Exception{
+        if(equation == null) throw new Exception("Bad input parameters");
+        int result = 0;
+
+
+
+        return result;
+    }
+}
